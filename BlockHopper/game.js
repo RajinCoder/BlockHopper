fbb158702b070cfd462a48729f360007057cbb71
RajinCoder <petercmoise@gmail.com>
2022-04-14 10:08:39 -0400

    Update game.js

--- BlockHopper/game.js
+++ BlockHopper/game.js
@@ -1,5 +1,7 @@
 var canvas = document.getElementById("gameCanvas");
-var ctx = canvas.getContext('2d');
+var ctx = canvas.getContext('2d'); //just lets it know we are working with 2d items
+var clouds = document.getElementById("cloud");
+
 
 var playerRadius = 15;
 var playerY = 0;
@@ -10,21 +12,32 @@ var enemyX = 0;
 var enemyHeight = 40;
 var enemyWidth = 20;
 
+var cloudsX = [];
+var cloudsY = [];
+var cloudsWidth = 100;
+var cloudsHeight = 60;
+var cloudSpeed = 0;
+
 var changeX = 0;
 var changeY = 0;
 var score = 0;
 var increment = 0;
 
-var jumping = true;
+var highScore = 0;
+var holdingScore;
+var messages;
+
+var jumping = true; //the logics
 var onGround = true;
 var over = false;
 var collide = false;
+var highScorecheck;
 var keys = [];
 
-window.addEventListener("keydown", function (e) {
+window.addEventListener("keydown", function (e) { // listening for the keypress 
     keys[e.keyCode] = true;
 });
-window.addEventListener("keyup", function (e) {
+window.addEventListener("keyup", function (e) { // listening for the key release
     keys[e.keyCode] = false;
 });
 
@@ -42,13 +55,16 @@ setInterval(loop, (1000 / 60));
 
 
 
-// Make the game over slide in
-// add high score feature
 
 function loop() {
     enemyX += changeX;
     playerY += changeY;
     score += increment // make sure to change to 0 when collision
+    cloudsX[0] += cloudSpeed;
+    cloudsX[1] += cloudSpeed;
+    cloudsX[2] += cloudSpeed;
+    cloudsX[3] += cloudSpeed;
+    restart();
     update();
     render();
 }
@@ -57,24 +73,31 @@ function init() { // intialization
     playerY = canvas.height - playerRadius;
     enemyX = canvas.width / 2;
     enemyY = canvas.height - 40;
-    changeX = -3; //speed
-    increment = 1;
+    cloudsX = [10, (canvas.width / 2) - 130, canvas.width / 2 + 30, canvas.width - 110];
+    cloudsY = [40, 5, 40, 30];
+    changeX = -4; //speed
+    cloudSpeed = -.25;
+    increment = 1; // for the score to increase
+    score = 0;
+    highScorecheck = false;
 
 }
 function update() {
     collision()
     gameOver(collide)
+    High()
     controls()
     enemyLoop()
+    cloudLoop()
 
     function collision() {
         if (playerY + playerRadius >= canvas.height) { // keeps ball in screen
             playerY = canvas.height - playerRadius;
         }
 
-        if ((playerX + playerRadius) >= enemyX || ((playerX - playerRadius) >= enemyX )) {
+        if ((playerX + playerRadius) >= enemyX || ((playerX - playerRadius) >= enemyX)) { //Makes a container around the enemy 
             if ((playerX + playerRadius) <= enemyX + enemyWidth || (playerX - playerRadius) <= enemyX + enemyWidth) {
-                if ((playerY + playerRadius) >= enemyY ) {
+                if ((playerY + playerRadius) >= enemyY) {
                     if ((playerY + playerRadius) <= enemyY + enemyHeight) {
                         collide = true;
                     }
@@ -90,19 +113,19 @@ function update() {
             changeY = 5; //bring them down
         }
 
-        if (onGround == false) {
+        if (onGround == false) { //putting some logic
             jumping = true;
         } else {
             jumping = false;
         }
 
-        if (keys[32] == true || keys[38] == true) {
+        if (keys[32] == true || keys[38] == true) { // checking if the space button or up button is hit
             if (jumping == false) {
                 changeY = -4; // jump
             }
         }
 
-        function ground() {
+        function ground() { // more logic
             if (playerY + playerRadius < canvas.height) {
                 return onGround = false;
             } else {
@@ -111,48 +134,95 @@ function update() {
             }
         }
     }
-    function gameOver(collide) {
+    function gameOver(collide) { //if hit do all these things to make it look like game is over
         if (collide == true) {
             changeX = 0;
             changeY = 0;
             increment = 0;
+            cloudSpeed = 0;
             over = true;
         }
     }
 
-    function enemyLoop() {
-        if (enemyX < 0) {
-            enemyX = canvas.width - enemyWidth;
+    function enemyLoop() { // if the player jumps the enemy and it leaves screen have it loop around back
+        if (enemyX + enemyWidth < 0) {
+            enemyX = canvas.width;
             changeX -= .25;
 
         }
     }
+
+    function cloudLoop() { // same thing as enemy to make it look like player is moving
+        for (i = 0; i < cloudsX.length - 1; i++) {
+            if (cloudsX[i] + cloudsWidth < 0) {
+                cloudsX[i] = canvas.width;
+            }
+        }
+    }
+
+    function High() { //holds the high score for as long as the browsers open
+
+        if (over == true) {
+            holdingScore = sessionStorage.getItem('highScore');
+            if (holdingScore < score) {
+                sessionStorage.setItem('highScore', score);
+                messages = `New High Score ${score}!!!`
+                highScorecheck = true; //makes it so that it doesn't immediatly go run it again in a milisecond
+            } else {
+                if (highScorecheck == false) {
+                    messages = `High Score ${holdingScore}`
+                }
+
+            }
+        }
+
+    }
 }
 
-function render() {
+function render() { // draws the stuff on the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height) // clears whole canvas
     drawPlayer();
     drawEnemy();
+    drawEnv();
     if (over == true) {
         ctx.font = "50px Comic Sans MS";
         ctx.fillStyle = "black";
-        ctx.fillText("Game Over", canvas.width - 450, canvas.height / 2)
+        ctx.fillText(`Game Over`, canvas.width - 450, canvas.height / 2) //Game Over text
+        ctx.font = "25px Comic Sans MS";
+        ctx.fillText(`Your score was ${score}`, canvas.width - 430, canvas.height / 2 + 30) //shows the score you ended with
+        ctx.font = "18px Comic Sans MS";
+        ctx.fillText(messages, canvas.width - 400, canvas.height / 2 + 60) // your highscore shows and a different message in case you beat the previous
+        ctx.font = "20px Comic Sans MS";
+        ctx.fillText("Press R to restart", canvas.width - 595, canvas.height - 375) // lets the player know how to restart without refreshing
     }
     ctx.font = "20px Comic Sans MS";
     ctx.fillStyle = "black";
-    ctx.fillText(`Score: ${score}`, canvas.width - 130, canvas.height - 380)
-    function drawEnemy() {
+    ctx.fillText(`Score: ${score}`, canvas.width - 130, canvas.height - 380) // will always show the score in the top right
+    function drawEnemy() { // draws the enemy
         ctx.beginPath();
-        ctx.fillStyle = "#FF0000";
+        ctx.fillStyle = "green";
         ctx.rect(enemyX, enemyY, enemyWidth, enemyHeight);
         ctx.fill()
         ctx.closePath();
     }
-    function drawPlayer() {
+    function drawPlayer() { // draws the player
         ctx.beginPath();
         ctx.fillStyle = "black";
         ctx.arc(playerX, playerY, playerRadius, 0, 2 * Math.PI);
         ctx.fill()
         ctx.closePath();
+    }
+    function drawEnv() { // draws the clouds because I already had an image
+        ctx.drawImage(clouds, cloudsX[2], cloudsY[2], cloudsWidth, cloudsHeight); //3
+        ctx.drawImage(clouds, cloudsX[0], cloudsY[0], cloudsWidth, cloudsHeight); //1
+        ctx.drawImage(clouds, cloudsX[1], cloudsY[1], cloudsWidth, cloudsHeight); //2
+        ctx.drawImage(clouds, cloudsX[3], cloudsY[3], cloudsWidth, cloudsHeight); //4
+    }
+}
+function restart() { // if the r button is hit and the game is over restart the whole thing
+    if (over == true && keys[82] == true) {
+        init()
+        collide = false;
+        over = false;
     }
 }
